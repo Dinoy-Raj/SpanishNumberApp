e95e82ee5c79775bc4522183a094bf5a173aaf31
Dinoy-Raj <dinoykraj@gmail.com>
2020-09-13 21:53:11 +0530

    created app

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: MyButton(),
+  ));
+}
+
+class MyButton extends StatefulWidget{
+
+@override
+  MyButtonState createState(){
+  return MyButtonState();
+}
+}
+
+class MyButtonState extends State<MyButton>{
+
+  int counter =0;
+  List<String>spanishNumbers = [
+    "uno",
+    "dos",
+    "tres",
+    "cuatro",
+    "cinco",
+    "seis",
+    "seite",
+    "ocho",
+    "nueve",
+    "dietz"
+
+  ];
+  String defaultText = " Spanish Numbers ";
+
+
+  void displaySNumbers(){
+    setState(() {
+      defaultText = spanishNumbers[counter];
+
+      if(counter<9){
+        counter=counter+1;
+      }else{
+        counter=0;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title:Text("Spanish Number App"),
+        backgroundColor: Colors.orange,
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(defaultText,style: TextStyle(fontSize: 30.0),),
+              Padding(padding: EdgeInsets.all(10.0),),
+              RaisedButton(
+               child: Text("Call Numbers" , style:TextStyle(
+                 color:Colors.white
+               )),
+               onPressed: displaySNumbers,
+                color: Colors.orange,
+                splashColor:Colors.yellow,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+
+
+
+}
